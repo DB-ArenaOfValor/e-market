6a7e1d9ba6ab067f9e3202fcc88a7a2ea6ca1042
Crystaleee <crystale.1031@gmail.com>
2018-04-28 01:39:50 -0400

    Read function in admin.html
    for all tables

---
+++ js/admin.js
@@ -0,0 +1,76 @@
+// the form selected
+var selectedTab = "product";
+
+window.onload = function() {
+    load(selectedTab);
+};
+
+// load one of the forms
+function load(tab) {
+    toggleSelected(tab);
+    ajax("POST", "find_" + tab + ".php", new FormData(), fill);
+    //////////////////////////////// for test
+    fill(
+        JSON.parse(
+            '[{"PID": "00000001","state":"0","brand": "Blueberry","model": "iPhone X","year": "2017-11-03","color": "navy red","use_time": "0.2","price": "832.14","image": "1"},{"PID": "00000002","brand": "Sony","model": "iPhone X","year": "2017-11-03","color": "black","use_time": "0.2","price": "832.14","image": "1"}]'
+        )
+    );
+}
+
+// fill table and filter
+function fill(array) {
+    var keys = fillTable(array);
+    fillFilter(keys);
+}
+
+// add table row for each item in the result
+function fillTable(array) {
+    // add table header
+    var thead = document.querySelector("thead");
+    thead.innerHTML = "";
+
+    var item = array[0];
+    var keys = [];
+    for (var key in item) {
+        keys.push(key);
+
+        var div = document.createElement("div");
+        div.innerHTML = key;
+        var th = document.createElement("th");
+        th.innerHTML = key + div.outerHTML;
+        thead.appendChild(th);
+    }
+
+    //add table content
+    var tbody = document.querySelector("tbody");
+    tbody.innerHTML = "";
+    for (var i = 0; array && i < array.length; i++) {
+        var a = array[i];
+        var row = document.createElement("tr");
+        for (var j in keys) {
+            var k = keys[j];
+            row.innerHTML += "<td contenteditable='true'>" + a[k] + "</td>";
+        }
+        tbody.appendChild(row);
+    }
+    return keys;
+}
+
+// add filter for each attribute
+function fillFilter(keys) {
+    var filter = document.querySelector(".filter");
+    for (var i in keys) {
+        filter.innerHTML +=
+            "<div class='catagory'><div class='title'>" +
+            keys[i] +
+            "</div><input  name='" +
+            keys[i] +
+            "' type='text'></div>";
+    }
+}
+
+// submit filter when form is changed and change table
+function submitFilter() {
+    var formData = new FormData(document.querySelector("form.filter"));
+    ajax("POST", "find_" + selectedTab + ".php", formData, fillTable);
+}
---
+++ js/util.js
@@ -0,0 +1,39 @@
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return "";
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+function ajax(method, url, data, callback) {
+    if (window.XMLHttpRequest) {
+        // code for IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttp = new XMLHttpRequest();
+    } else {
+        // code for IE6, IE5
+        xmlhttp = new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            console.log(JSON.parse(this.responseText));
+            callback && callback(JSON.parse(this.responseText));
+            return this.responseText;
+        }
+    };
+    xmlhttp.open(method, url, true);
+    xmlhttp.send(data);
+}
+
+function resetForm() {
+    var form = document.querySelector("form");
+    form.reset();
+}
+
+function toggleSelected(tab) {
+    document.querySelector("." + selectedTab).classList.toggle("selected");
+    document.querySelector("." + tab).classList.toggle("selected");
+    selectedTab = tab;
+}
